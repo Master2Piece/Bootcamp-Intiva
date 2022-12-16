8bcfc9780e6497a1caa762b1837a82b81d335159
Master2Piece <chovenanthza.september@gmail.com>
2022-12-16 09:34:34 +0700

    pertama commit

---
+++ aritamtik.php
@@ -0,0 +1,13 @@
+<?php 
+$a = 3;
+$b = 4;
+
+echo "Bilangan 1 = ",$a, "<br>";
+echo "Bilangan 2 = ",$b, "<br>";
+echo "<br>";
+echo "Menampilkan output menggunakan perintah 'echo' <br>";
+echo "Perkalian = ",$a+$b,"<br>";
+echo "Perkalian = ",$a-$b,"<br>";
+echo "Perkalian = ",$a*$b,"<br>";
+echo "Perkalian = ",$a/$b,"<br>";
+?>
